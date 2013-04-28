6cea31b6da693a32ccd099cdbb101d34dd420fe4
werner <wvesteraas@gmail.com>
2013-04-28 21:22:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./lib/aarlogic_gps_3t.js');
---
+++ test/aarlogic_gps_3t.test.js
@@ -0,0 +1,15 @@
+var aarlogic_gps_3t = require('../lib/aarlogic_gps_3t.js');
+
+
+//console.log(aarlogic_gps_3t.parse('$GPGGA,170834,4124.8963,N,08151.6838,W,1,05,1.5,280.2,M,-34.0,M,,,*59'));
+//console.log(aarlogic_gps_3t.parse('$GPGGA,105702.000,5955.5612,N,01042.7994,E,1,05,3.6,65.6,M,40.7,M,,0000*63'));
+//console.log(aarlogic_gps_3t.parse('$GPGLL,4250.5589,S,14718.5084,E,092204.999,A*2D'));
+//console.log(aarlogic_gps_3t.parse('$GPRMC,110239.420,A,5955.5692,N,01042.7911,E,4.97,72.78,280413,,,A*51'));
+//console.log(aarlogic_gps_3t.parse('$GPGSA,A,3,28,07,10,08,,,,,,,,,6.5,5.3,3.9*39'));
+//console.log(aarlogic_gps_3t.parse('$GPGSA,A,3,,,,,,16,18,,22,24,,,3.6,2.1,2.2*3C'));
+
+//console.log(aarlogic_gps_3t.parse('$GPGSV,3,1,12,08,72,095,26,07,42,071,33,28,33,150,37,03,15,030,*74'));
+//console.log(aarlogic_gps_3t.parse('$GPGSV,3,2,12,10,14,170,31,26,59,273,19,05,50,217,,09,46,228,*7A'));
+//console.log(aarlogic_gps_3t.parse('$GPGSV,3,3,12,15,19,285,,21,16,328,,19,13,052,26,06,11,012,*78'));
+
+console.log(aarlogic_gps_3t.parse('$GPVTG,,T,,M,0.00,N,0.0,K,A*13'));
